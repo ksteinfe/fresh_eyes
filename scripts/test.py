2a522f629c0ce479451a5e04143e1c0af425ee7e
Kyle Steinfeld <ksteinfe@gmail.com>
2019-09-07 13:24:56 -0700

    BIG PURGE of unrealted scripts

--- scripts/test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# env variable must be set in the anaconda environment, as per https://conda.io/docs/user-guide/tasks/manage-environments.html#saving-environment-variables
-DATA_PATH = os.environ['FRESH_EYES_DATA_PATH']
-PRIMARY_DATA_PATH = os.path.join(DATA_PATH,'datasets_primary')
-
-geojson_path = os.path.join(PRIMARY_DATA_PATH,'microsoft_us_building_footprints','geojson')
-
-print(os.listdir(geojson_path))
